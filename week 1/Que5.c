37be45492a8b8b7ffff646f8e759f27d57cf2186
Uttam kumar <uttam9435@gmail.com>
2021-10-25 21:21:57 +0530

    quick update

---
+++ week 1/Que5.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main()
+{
+    int x,y,z;
+    printf("enter three nos");
+    scanf("%d %d %d",&x,&y,&z);
+    if(x>y)
+    {
+        printf("no x is greater than no y",x);
+    }
+    else if(y>z)
+    {
+        printf("no y is greater than no z",y);
+    }
+    else{
+        printf("no z is greater than x & y",z);
+    }
+}
