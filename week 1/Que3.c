7377afec71af6c8cdb381552ec951c33433e1aeb
Uttam kumar <uttam9435@gmail.com>
2021-10-25 23:48:23 +0530

    quick update

---
+++ week 1/Que3.c
@@ -0,0 +1,15 @@
+/*wap to write a program no is even or odd
+#include<stdio.h>
+void main()
+{
+    int num;
+    printf("enter a no");
+    scanf("%D",&num);
+    if{
+        num%2==0;
+        printf("number is even",num);
+    }
+    else{
+        printf("number is odd",num);
+    }
+}
