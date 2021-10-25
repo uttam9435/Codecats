a6acbd578ec1f78bcad6110affb8adca8e630b66
Uttam kumar <uttam9435@gmail.com>
2021-10-26 00:18:11 +0530

    quick update

---
+++ week 1/Que8.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+void main()
+{
+    int num;
+    printf("enter weak number");
+    scanf("%d",&num);
+    if(num==1)
+    {
+        printf("monday");
+    }
+    else if(num==2)
+    {
+        printf("tuesday");
+    }
+    else if(num==3)
+    {
+        printf("wednesday");
+    }
+    else if(num==4)
+    {
+        printf("thrusday");
+    }
+    else if(num==5)
+    {
+        printf("friday");
+    }
+    else if(num==6)
+    {
+        printf("saturday");
+    }
+    else if(num==7)
+    {
+        printf("sunday");
+    }
+    else
+    {
+        printf("wrong number pls enter number between 1-7");
+    }
+}
